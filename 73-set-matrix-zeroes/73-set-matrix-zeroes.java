7c25745b7283b7c6cd647937695c7b80e7bfab32
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-07-13 22:32:39 +0530

    Time: 1 ms (96.99%), Space: 54.3 MB (28.49%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,23 +1,27 @@
 class Solution {
-    public void setZeroes(int[][] mat) {
-//       int m=matrix[0].length  ,n=matrix.length;
-//         boolean r=false,c=false;
-//         for (int j=0;j<m;j++){
-//             if(matrix[0][j]==0){
-//                 r=true;break;}
-//         }
-//             for (int i=0;i<n;i++){
-//             if(matrix[i][0]==0){
-//                 c=true;break;}
-//         }
-//         for (int i=0;i<n;i++){
-//              for (int j=0;j<m;j++){
-//                 if(matrix[i][j]==0){ 
-//                     matrix[i][0]=0;
-//                     matrix[0][j]=0;
-//                 }
-//              }
-//         }
+    public void setZeroes(int[][] matrix) {
+      int m=matrix[0].length  ,n=matrix.length;
+        boolean r=false,c=false;
+        for (int j=0;j<m;j++){
+            if(matrix[0][j]==0){
+                r=true;break;}
+        }
+            for (int i=0;i<n;i++){
+            if(matrix[i][0]==0){
+                c=true;break;}
+        }
+        for (int i=0;i<n;i++){
+             for (int j=0;j<m;j++){
+                if(matrix[i][j]==0){ 
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+             }
+        }
+                for (int i = 1; i < matrix.length; i++)
+            for (int j = 1; j < matrix[0].length; j++)
+                if (matrix[0][j] == 0 || matrix[i][0] == 0)
+                    matrix[i][j] = 0;
 //         for (int i=0;i<n;i++){
 //              if(matrix[i][0]==0){
 //                  for(int j=1;j<m;j++){
@@ -33,48 +37,49 @@ class Solution {
 //                  }
 //              }
 //         }
-//         if(c==true){
-//             for(int i=1;i<n;i++){
-//                 matrix[i][0]=0;
-//             }
-//         }
-//           if(r==true){
-//             for(int j=1;j<m;j++){
-//                 matrix[0][j]=0;
-//             }
-//         }
-       // return matrix;
-
-        boolean row_flag = false;
-        boolean col_flag = false;
- 
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[0].length; j++) {
-                if (i == 0 && mat[i][j] == 0)
-                    row_flag = true;
- 
-                if (j == 0 && mat[i][j] == 0)
-                    col_flag = true;
- 
-                if (mat[i][j] == 0) {
- 
-                    mat[0][j] = 0;
-                    mat[i][0] = 0;
-                }
+        if(c==true){
+            for(int i=1;i<n;i++){
+                matrix[i][0]=0;
             }
         }
- 
-        for (int i = 1; i < mat.length; i++)
-            for (int j = 1; j < mat[0].length; j++)
-                if (mat[0][j] == 0 || mat[i][0] == 0)
-                    mat[i][j] = 0;
- 
-        if (row_flag == true)
-            for (int i = 0; i < mat[0].length; i++)
-                mat[0][i] = 0;
- 
-        if (col_flag == true)
-            for (int i = 0; i < mat.length; i++)
-                mat[i][0] = 0;
+          if(r==true){
+            for(int j=1;j<m;j++){
+                matrix[0][j]=0;
+            }
+        }
     }
+       // return matrix;
+
+//         boolean row_flag = false;
+//         boolean col_flag = false;
+ 
+//         for (int i = 0; i < mat.length; i++) {
+//             for (int j = 0; j < mat[0].length; j++) {
+//                 if (i == 0 && mat[i][j] == 0)
+//                     row_flag = true;
+ 
+//                 if (j == 0 && mat[i][j] == 0)
+//                     col_flag = true;
+ 
+//                 if (mat[i][j] == 0) {
+ 
+//                     mat[0][j] = 0;
+//                     mat[i][0] = 0;
+//                 }
+//             }
+//         }
+ 
+//         for (int i = 1; i < mat.length; i++)
+//             for (int j = 1; j < mat[0].length; j++)
+//                 if (mat[0][j] == 0 || mat[i][0] == 0)
+//                     mat[i][j] = 0;
+ 
+//         if (row_flag == true)
+//             for (int i = 0; i < mat[0].length; i++)
+//                 mat[0][i] = 0;
+ 
+//         if (col_flag == true)
+//             for (int i = 0; i < mat.length; i++)
+//                 mat[i][0] = 0;
+//     }
 }
