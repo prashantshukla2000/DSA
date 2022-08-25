db93bcaf07943f6f56b587ef5ecfa5a1179c9e35
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-08-26 02:06:43 +0530

    Time: 12 ms (85.94%), Space: 120.2 MB (52.45%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -4,7 +4,6 @@ class Solution {
         int[] seen = new int[n];// to set the indegree
         for (List<Integer> e: edges) //we check for each pair
             seen[e.get(1)] = 1; //we set the indegree of the destination as 1.
-    List<Integer> al = new ArrayList<>();
     for (int i=0; i<seen.length; i++) { // if indegree is 0 add in arraylit
         if (seen[i] == 0) res.add(i);
     }
