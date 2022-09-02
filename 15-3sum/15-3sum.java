7420af586293bdb1a103cc5851b68f546081382f
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-02 09:19:18 +0530

    Time: 27 ms (83.22%), Space: 46.5 MB (92.92%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,34 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> li =new ArrayList<>();
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(i>0 && nums[i]==nums[i-1])
+     Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<List<Integer>>();
+        
+                for(int i=0;i<nums.length;i++){
+                if(i>0 && nums[i]==nums[i-1])
                 continue;
-            else{
-                int l=i+1,r=nums.length-1;
-                while(l<r){
-                    int threesum=nums[l]+nums[r]+nums[i];
-                    if(threesum<0)
-                        l++;
-                    else if(threesum>0)
-                        r--;
-                    else 
-                    {       List<Integer> al =new ArrayList<>();
-                               al.add(nums[i]);
-                                al.add(nums[l]);
-                               al.add(nums[r]);
-                     li.add(al);
-
-                        l++;
-                        while(l<r && nums[l]==nums[l-1])
+                    else{
+                    int j=i+1,k=nums.length-1;
+                while(j<k){
+                    if(nums[i]+nums[j]+nums[k]==0 && i!=j&& j!=k && k!=i)
+                    {
+                        List<Integer> li=new ArrayList<Integer>();
+                        li.add(nums[i]);li.add(nums[j]);li.add(nums[k]);
+                        ans.add(li);
+                        j++;
+                         while(j<k && nums[j]==nums[j-1])
                         {
-                            l++;
+                            j++;
+                        }
+                    }else{
+                        if(nums[i]+nums[j]+nums[k]>0 ){
+                            k--;
+                        }else{
+                            j++;
                         }
                     }
                 }
-            }
-        }
-        return li;
+                    }
+                }        
+            return ans;
     }
 }
