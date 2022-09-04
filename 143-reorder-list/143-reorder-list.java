b3d8e4af4b38ee4ca7c8955019ebdec6a1b56a78
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-05 05:07:30 +0530

    Time: 8 ms (14.77%), Space: 53.7 MB (67.77%) - LeetHub

--- 143-reorder-list/143-reorder-list.java
+++ 143-reorder-list/143-reorder-list.java
@@ -10,50 +10,47 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        if(head==null||head.next==null)
+         if(head==null||head.next==null)
             return ;
-        ListNode h=cutinhalf(head);
-        h=reverse(h);
-      ListNode p=head;
+      ListNode back=cut(head);
+        back=Reverse(back);
+        ListNode front=head;
         
-        while(h!=null){
-           ListNode x=h.next;
-            h.next=p.next;
-            p.next=h;
-            p=p.next.next;
-            h=x;
+         while(back!=null){
+           ListNode x=back.next;
+           back.next=front.next;
+           front.next=back;
+            front=front.next.next;
+             back=x;
         }
-       
-        
+
     }
-    public ListNode cutinhalf(ListNode head){
-        ListNode p1=head,p2=head.next;
-        while(p2.next!=null&&p2.next.next!=null){
-            p1=p1.next;
-            p2=p2.next.next;
+    public ListNode cut(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast.next!=null &&fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        if(p2.next!=null){     //for odd we will break at one more.
-            p1=p1.next;
+        if(fast.next!=null){
+            slow=slow.next;
         }
-        ListNode t=p1.next;
-        p1.next=null;
+        ListNode t=slow.next;
+        slow.next=null;
         return t;
-        
     }
-    public ListNode reverse(ListNode head){
-        if(head==null||head.next==null)
+    public ListNode Reverse(ListNode head){
+        if(head==null ||head.next==null)
             return head;
-        ListNode p=head,c=head.next,n=head.next.next;
-        while(n!=null)
-        {
-            c.next=p;
-            p=c;
-            c=n;
-            n=n.next;
+        ListNode a=head;
+        ListNode b=head.next;
+        ListNode c=head.next.next;
+        while(c!=null){
+            b.next=a;
+            a=b;b=c;c=c.next;
         }
         head.next=null;
-        c.next=p;
-        return c;
+        b.next=a;
+        return b;
     }
-
 }
