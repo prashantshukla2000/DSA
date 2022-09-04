87f85c22f32e294cda138a2c105371a87754f720
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-05 03:20:28 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (94.78%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -8,26 +8,58 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+//CODE HERE 
+// ListNode temp=head;
+// int count=0;
+// while(temp!=null){
+//     count++;
+//     temp=temp.next;
+// }
+// ListNode temp1=new ListNode(0);
+// temp1.next=head;
+// ListNode tmp2=temp1;int i=0;
+// while(temp1!=null){
+// if((count-n)==i){
+//     temp1.next=temp1.next.next;
+// }else{
+//     temp1=temp1.next;
+// }
+// i++;
+// }
+// return tmp2.next;
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-//CODE HERE 
-ListNode temp=head;
-int count=0;
-while(temp!=null){
-    count++;
-    temp=temp.next;
+if(head==null||head.next==null)
+    return null;
+ListNode a=new ListNode(0,Reverse(head));
+        ListNode temp=a;
+        int i=0;
+        while(a!=null){
+            if(i==n-1){
+                a.next=a.next.next;
+            }
+            else{
+            a=a.next;
+            }
+            i++;
+        }
+        
+        return Reverse(temp.next);
 }
-ListNode temp1=new ListNode(0);
-temp1.next=head;
-ListNode tmp2=temp1;int i=0;
-while(temp1!=null){
-if((count-n)==i){
-    temp1.next=temp1.next.next;
-}else{
-    temp1=temp1.next;
-}
-i++;
-}
-return tmp2.next;
-}
+    public ListNode Reverse(ListNode head){
+        if(head==null||head.next==null)
+            return head;
+    ListNode a=head;
+    ListNode b=head.next;
+    ListNode c=head.next.next;
+        while(c!=null){
+            b.next=a;
+            a=b;
+            b=c;
+            c=c.next;
+        }
+        head.next=null;
+        b.next=a;
+        return b;       
+    }
     }
