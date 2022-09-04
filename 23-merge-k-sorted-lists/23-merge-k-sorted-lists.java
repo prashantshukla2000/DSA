f284f66ef7e1205b6f50338ebb237adf0cf1e387
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-05 03:57:57 +0530

    Time: 114 ms (20.12%), Space: 44.3 MB (89.96%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,23 +10,37 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists==null ||lists.length==0)
-            return null;
-      PriorityQueue <Integer> queue=new PriorityQueue<>();
-for(ListNode head:lists){
-while(head!=null){
-queue.add(head.val);
-head=head.next;
-}
-}
+if (lists == null || lists.length == 0) {
+    return null;
+  }
+      for (int i = 1; i < lists.length; ++i) {
+      lists[0] = mergeTwoLists(lists[0], lists[i]);
+  }
 
-   ListNode dummy=new ListNode(0);
-   ListNode temp=dummy;
-while(!queue.isEmpty())
-{temp.next=new ListNode(queue.poll());
-temp=temp.next;
-}
-return dummy.next;
-
+  return lists[0];
+        }
+    
+      public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode l=new ListNode(0);
+        ListNode l3=l;
+    
+        while(l1!=null && l2 !=null){
+            if(l1.val<=l2.val){
+                l3.next=l1;
+                l1=l1.next;
+            }else{
+                 l3.next=l2;
+                 l2=l2.next;
+            }
+            l3=l3.next;
+        }
+    if (l1 != null) {
+            l3.next = l1;
+        }
+          else if (l2 != null) {
+            l3.next = l2;
+        }
+        return l.next;
     }
+
 }
