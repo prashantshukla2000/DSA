b1589d9be2b7bc6da961120b4a5568b234aa2c0c
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-09 12:41:12 +0530

    Time: 43 ms (43.88%), Space: 43.4 MB (44.92%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        
+      HashMap<Node,Node> oldtonew=new HashMap<>();
+        return dfs(node,oldtonew);
+    }
+    public Node dfs(Node node,HashMap<Node,Node> oldtonew){
+        if(node==null)
+            return null;
+        if(oldtonew.containsKey(node)){
+            return oldtonew.get(node);
+        }
+        Node copy=new Node(node.val);
+        oldtonew.put(node,copy);
+        for(Node i: node.neighbors){
+          copy.neighbors.add(dfs(i,oldtonew));
+        }
+        return copy;
+    }
+}
