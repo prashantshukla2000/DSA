9bd80b0840fbb091bdce6e289a295d9a4a5cf66c
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-09 07:49:34 +0530

    Time: 10 ms (96.37%), Space: 44.8 MB (95.04%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,21 +1,19 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> hm= new HashMap<Integer,Integer>();
-        for(int i=0;i<nums.length;i++){
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+       HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i:nums){
+            hm.put(i,hm.getOrDefault(i,0)+1);
         }
-        //maxheap
-        PriorityQueue<Integer> queue =new PriorityQueue<Integer>((a,b)->
-            hm.get(b)-hm.get(a));
+         PriorityQueue<Integer> queue =new PriorityQueue<Integer>
+((a,b)-> hm.get(b)-hm.get(a));
         for(int key:hm.keySet()){
             queue.add(key);
         }
-       // System.out.print(hm);
-        int result[]=new int[k];
-        for(int i=0;i<k;i++)
-        {
-            result[i]=queue.poll();
+                        int res[]=new int[k];
+        for(int i=0;i<k;i++){
+            res[i]=queue.poll();
         }
-        return result;
+        return res;
     }
-}
+ }
+
