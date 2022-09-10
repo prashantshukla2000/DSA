bdbbb0e31c40133bf5baefd3e972ae67ed241b25
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-10 07:21:44 +0530

    Time: 929 ms (5.02%), Space: 151.6 MB (31.03%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,26 +1,21 @@
+//if any no doesn't have a left neighbour like for 2 left neighbour is 2-1=1 then it is said to start of the sequence in ex={100,4,1,2,200,3} ,...start of sequence are 1,100,200 since they ahve no left no. ..for these we check which of then can form a ongest sequence by adding 1 and checking in set ,others that are not sequence starter we skip them as they would be the right neighbour of any one of the start elementss
 class Solution {
     public int longestConsecutive(int[] nums) {
- if (nums.length == 0 || nums.length  == 1){
-            return nums.length;
+      Set<Integer> set=new HashSet<Integer>();
+        int longest=0;
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
         }
-        Arrays.sort(nums);
-        int count = 1;
-        int max_count = 0;
-        for (int i = 1; i < nums.length; i++){
-            if (nums[i] - nums[i-1] == 1){
-                count++;
-                
-            }
-            else if(nums[i] - nums[i-1] == 0){
-            }
-            else{
-                count = 1;
-            }
-           
-            if (count > max_count){
-                max_count = count;
+        for(int i :nums){
+            if(!set.contains(i-1)){
+                int count=0,a=0;
+                while(set.contains(i+a)){
+                    count=count+1;
+                    a=a+1;
+                    longest=Math.max(count,longest);
+                }
             }
         }
-        return max_count;
+        return longest;
     }
 }
