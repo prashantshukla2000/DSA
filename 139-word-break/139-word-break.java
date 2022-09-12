cca06f8c3f4df0a60f242adf7af34762823e4202
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-12 10:08:39 +0530

    Time: 3 ms (94.13%), Space: 42.3 MB (91.35%) - LeetHub

---
+++ 139-word-break/139-word-break.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+      boolean[] dp=new boolean [s.length()+1];
+        for(int i=0;i<s.length();i++){
+            dp[i]=false;
+        }
+        dp[s.length()]=true;
+        for(int i=s.length()-1;i>=0;i--){
+            for(String j :wordDict){
+                if(i+j.length()<=s.length() && s.substring(i,i+j.length()).equals(j)){
+                    dp[i]=dp[i+j.length()];
+                }
+                if(dp[i]==true)
+                    break;
+            }
+        }
+        return dp[0];
+    }
+}
