d491956df0e67f6e1a7c4ecfbd05d665be924089
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-13 10:16:14 +0530

    Time: 4 ms (21.61%), Space: 42.7 MB (30.90%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,28 +1,23 @@
 class Solution {
-    HashMap<Integer,Integer> dp=new HashMap<>();
+    HashMap<Integer,Integer> dp=new HashMap<>(); //index of character ,the ways it can be decoded
     int res;
     public int numDecodings(String s) {
-        int size = s.length();
+         int size = s.length();
         dp.put(size, 1); //if we get an empty string w want to reurn 1
-        return dfs(s, 0); 
-    }
-        private int dfs(String s, int i) {
-        int size = s.length();
-        
-        if (dp.containsKey(i)) {
-            return dp.get(i);
-        }
+        for(int i=s.length()-1;i>-1;i--){
         if (s.charAt(i) == '0') {
-            return 0;
+            dp.put(i, 0);
+        }else{
+        dp.put(i,dp.get(i+1)) ;
         }
-        res = dfs(s, i + 1);
-        if ((i + 1 < size) && ((s.charAt(i) == '1') 
+            if ((i + 1 < size) && ((s.charAt(i) == '1') 
             || (s.charAt(i) == '2') && (Character.getNumericValue(s.charAt(i + 1)) < 7))
             ) {
-            res += dfs(s, i + 2);
+       int ans= dp.get(i)+dp.get(i+2) ;
+                dp.put(i,ans);
         }
-        dp.put(i, res);
-        return res;
+        }
+
+        return dp.get(0); 
     }
 }
-
