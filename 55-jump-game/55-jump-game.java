8f8dca56540ddd7bd1f94e4cfb5b3513b8642907
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-13 14:12:14 +0530

    Time: 1 ms (100.00%), Space: 68.4 MB (27.26%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal=nums.length-1;
+        for(int i=nums.length-1;i>=0;i--){
+            if(i+nums[i]>=goal )
+                goal=i;
+        }
+        if(goal==0)
+            return true;
+        else
+            return false;
+    }
+}
