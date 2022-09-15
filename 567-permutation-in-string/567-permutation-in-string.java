b7bc63a0f38b6a46e5d8b76c3fbbd216f3925ca2
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-16 00:44:30 +0530

    Time: 26 ms (42.78%), Space: 43.5 MB (50.39%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -6,14 +6,13 @@ class Solution {
         //add the values to the hash array
         for (int i = 0; i < s1.length(); i++) {
             arr[s1.charAt(i) - 'a']++;
+            arr[s2.charAt(i) - 'a']--;
+
         }
         int i = 0;
-        int j = 0;
+        int j = s1.length()-1;
         //point j to it's position
-        for (; j < s1.length(); j++) {
-            arr[s2.charAt(j) - 'a']--;
-        }
-        j--;
+
         if (isEmpty(arr)) return true;
         while (j < s2.length()) {
             arr[s2.charAt(i) - 'a']++;
