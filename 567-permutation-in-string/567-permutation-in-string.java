0da557263f9ad931d53fa754dcd3b1715c6429a3
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-19 12:19:58 +0530

    Time: 23 ms (46.23%), Space: 42.4 MB (89.32%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,24 +1,25 @@
 class Solution {
-
     public boolean checkInclusion(String s1, String s2) {
-        if (s2.length() < s1.length()) return false;
+            if (s2.length() < s1.length()) return false;
         int[] arr = new int[26];
         //add the values to the hash array
         for (int i = 0; i < s1.length(); i++) {
             arr[s1.charAt(i) - 'a']++;
-            arr[s2.charAt(i) - 'a']--;
-
         }
-        int i = 0;
-        int j = s1.length()-1;
-        //point j to it's position
-
+        int i = 0; //left pointer
+        int j = 0; //right pointer
+        //point j to it's position 
+        for (; j < s1.length(); j++) {
+            arr[s2.charAt(j) - 'a']--;
+        }
+        j--;//to point at s1.legth()-1..as inc after loop
         if (isEmpty(arr)) return true;
         while (j < s2.length()) {
             arr[s2.charAt(i) - 'a']++;
             i++;
             j++;
             if (j < s2.length()) arr[s2.charAt(j) - 'a']--;
+
             if (isEmpty(arr)) return true;
         }
         return isEmpty(arr);
@@ -31,3 +32,5 @@ class Solution {
         return true;
     }
 }
+
+ 
