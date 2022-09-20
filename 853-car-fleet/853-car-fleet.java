a39dc92e2b824466f52593bf9c9fb2bfb3ebaf85
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-20 19:27:16 +0530

    Time: 69 ms (97.31%), Space: 59.9 MB (86.38%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.java
@@ -0,0 +1,23 @@
+//we go from las to first car after sorting as if we go from from starting to end speed of the car could have been changed bec. of the car infront of it,so we go grom last to first
+//w sort them as then only the car whic has covered more ground then it intially will be present after it
+//time..0(nlogn)...o(space)
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+         if (position.length == 1) return 1;
+        HashMap<Integer,Integer> hm=new    HashMap<Integer,Integer> ();
+        Stack<Double> stack=new Stack<Double>();
+        for(int i=0;i<position.length;i++){
+            hm.put(position[i],speed[i]);
+        }
+        Arrays.sort(position);
+        for(int i=position.length-1;i>=0;i--){
+            double currentTime = (double)(((double)target -position[i] )/hm.get(position[i]));
+             if(!stack.isEmpty()&& currentTime<=stack.peek()){
+                       continue;
+            }else{
+                        stack.push(currentTime);
+            }
+        }
+       return stack.size();
+    }
+}
