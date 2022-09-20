7f4cbc641645cc600d5f4665221c5c16c9f92faa
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-20 15:18:44 +0530

    Time: 16 ms (18.13%), Space: 44.3 MB (38.55%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -1,17 +1,23 @@
 class Solution {
-        private Set<String> ops = new HashSet<>(Arrays.asList("+", "-", "*", "/"));
+
     public int evalRPN(String[] tokens) {
-     Stack<Integer>stack=new Stack<Integer>();
-        for(String t:tokens){
-         if (!ops.contains(t)) stack.push(Integer.parseInt(t));
-            else {
-                int b = stack.pop(), a = stack.pop();
-                if (t.equals("+")) stack.push(a + b);
-                else if (t.equals("-")) stack.push(a - b);
-                else if (t.equals("*")) stack.push(a * b);
-                else stack.push(a / b);
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if (token.equals("+")) {
+                stack.add(stack.pop() + stack.pop());
+            } else if (token.equals("-")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.add(b - a);
+            } else if (token.equals("*")) {
+                stack.add(stack.pop() * stack.pop());
+            } else if (token.equals("/")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.add(b / a);
+            } else {
+                stack.add(Integer.parseInt(token));
             }
-            
         }
         return stack.pop();
     }
