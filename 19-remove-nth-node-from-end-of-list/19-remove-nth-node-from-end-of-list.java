ea5b1335c76bd9534830d27f1a34a36d309d16af
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-23 21:59:37 +0530

    Time: 1 ms (74.84%), Space: 42.5 MB (34.93%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -8,58 +8,33 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-//CODE HERE 
-// ListNode temp=head;
-// int count=0;
-// while(temp!=null){
-//     count++;
-//     temp=temp.next;
-// }
-// ListNode temp1=new ListNode(0);
-// temp1.next=head;
-// ListNode tmp2=temp1;int i=0;
-// while(temp1!=null){
-// if((count-n)==i){
-//     temp1.next=temp1.next.next;
-// }else{
-//     temp1=temp1.next;
-// }
-// i++;
-// }
-// return tmp2.next;
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-if(head==null||head.next==null)
-    return null;
-ListNode a=new ListNode(0,Reverse(head));
-        ListNode temp=a;
+       ListNode temp=new ListNode(0,reverse(head));
+        ListNode t=temp;
         int i=0;
-        while(a!=null){
-            if(i==n-1){
-                a.next=a.next.next;
-            }
-            else{
-            a=a.next;
-            }
+        while(i<n-1){
+         
+                temp=temp.next;
             i++;
         }
-        
-        return Reverse(temp.next);
-}
-    public ListNode Reverse(ListNode head){
-        if(head==null||head.next==null)
+            temp.next=temp.next.next;
+        return reverse(t.next);
+    }
+     public ListNode reverse(ListNode head){
+          if(head==null ||head.next==null)
             return head;
-    ListNode a=head;
-    ListNode b=head.next;
-    ListNode c=head.next.next;
-        while(c!=null){
-            b.next=a;
-            a=b;
-            b=c;
-            c=c.next;
+
+        ListNode prev=head,curr=head.next,nex=head.next.next;
+        while(nex!=null){
+            curr.next=prev;
+            prev=curr;
+            curr=nex;
+            nex=nex.next;
         }
+        curr.next=prev;
         head.next=null;
-        b.next=a;
-        return b;       
+        return curr;
     }
-    }
+    
+}
