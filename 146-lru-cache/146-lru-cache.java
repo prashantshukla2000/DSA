f2452e2f475ebf61bc9ba975ee6b33976909cdb0
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-24 18:33:28 +0530

    Time: 116 ms (36.04%), Space: 129.5 MB (37.96%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,63 +1,59 @@
 class Node{
     int key;
     int val;
-    Node next;
     Node prev;
+    Node next;
     public Node(int key,int val){
         this.key=key;
         this.val=val;
     }
 }
-
-
 class LRUCache {
-Map<Integer,Node> mp=null;
-Node head=new Node(-1,-1);
-Node tail=new Node(-1,-1);
-int capacity=0;
-
+  HashMap<Integer,Node> map=null;
+    Node head=new Node(-1,-1);
+    Node tail=new Node(-1,-1);
+    int capacity=0;
+    
     public LRUCache(int capacity) {
-    mp=new HashMap<Integer,Node>();
-    join(head,tail);
-    this.capacity=capacity;
-        
+      this.capacity=capacity;
+        map=new HashMap<>();
+        join(head,tail);
     }
-    public void join(Node n1,Node n2){
-    n1.next=n2;
-    n2.prev=n1;
-}
-
-public void remove(Node node){
-    node.prev.next=node.next;
-    node.next.prev=node.prev;
-}
-    public void moveToHead(Node head,Node node){
-    Node next=head.next;
-    join(head,node);
-    join(node,next);
-}
+    public void join(Node n1,Node n2 ){
+        n1.next=n2;
+        n2.prev=n1;
+    }
+    public void remove(Node node){
+        node.prev.next=node.next;
+        node.next.prev=node.prev;
+    }
+    public void movetohead(Node head,Node node){//ensuring that the dummy node is in its place and to insert node to the front after removing it.
+        Node next=head.next;
+        join(head,node);
+        join(node,next);
+    }
     
     public int get(int key) {
-         if(!mp.containsKey(key)){
-        return -1;
-    }
-    Node node=mp.get(key);
-    remove(node);
-    moveToHead(head,node);
-    return node.val;
+        if(!map.containsKey(key))
+            return -1;
+        Node node=map.get(key); 
+        remove(node);
+        movetohead(head,node);//we add to the head of the link list as it is the most recently used node so it must be in the list at the top
+        return node.val;
     }
     
-    public void put(int key, int val) {
-         if(mp.containsKey(key)){
-        Node node=mp.get(key);
-        remove(node);
-    }else if(mp.size()==capacity){
-        mp.remove(tail.prev.key);
+    public void put(int key, int value) {
+       if(map.containsKey(key)){//if found remove so,add the new value as a new key-value pair
+          Node node=map.get(key);
+           remove(node);
+       } 
+        else if(map.size()==capacity){ //if capacity is reched we rmove the last node ,ie,one before the last dummy
+        map.remove(tail.prev.key);
         remove(tail.prev);
-    }
-    Node node=new Node(key,val);
-    moveToHead(head,node);
-    mp.put(key,node);
+        }
+        Node newNode=new Node(key,value);
+        movetohead(head,newNode);
+        map.put(key,newNode);
     }
 }
 
