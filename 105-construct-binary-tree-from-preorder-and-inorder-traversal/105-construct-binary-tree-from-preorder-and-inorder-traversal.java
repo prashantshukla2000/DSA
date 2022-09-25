5a32d6985e7882104f25f603cbef051b35bcbda9
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-25 14:04:51 +0530

    Time: 3 ms (89.10%), Space: 44.9 MB (25.93%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,32 +14,31 @@
  * }
  */
 class Solution {
-    HashMap<Integer,Integer> hm=new HashMap<Integer,Integer>();
-    int pre[];
+    HashMap<Integer,Integer> map=new HashMap();//for inorder
+    int pre[];//for preorder
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         pre=preorder;
         for(int i=0;i<inorder.length;i++){
-            hm.put(inorder[i],i);
+           map.put(inorder[i],i);
         }
-       return helper(0,preorder.length-1,0,preorder.length-1);
+       return dfs(0,inorder.length-1,0,preorder.length-1);
     }
-   public TreeNode  helper(int prestart,int preend ,int instart,int inend){
-       int index=hm.get(pre[prestart]);
-       int l=index-instart;
-       int r=inend-index;
-       TreeNode root=new TreeNode(pre[prestart]);
-       if(l!=0){
-          root.left= helper(prestart+1,prestart+l,instart,index-1);
-           
-       }else{
-           root.left=null;
-       }
-              if(r!=0){
-          root.right= helper(prestart+l+1,preend,index+1,inend);
-           
-       }else{
-           root.right=null;
-       }
-       return root;
-   }
+        
+      public TreeNode  dfs(int instart,int inend,int prestart,int preend){
+        int index=map.get(pre[prestart]);
+          int  l=index-instart;
+         int  r=inend-index;
+          TreeNode root=new TreeNode(pre[prestart]);
+          if(l!=0){
+              root.left=dfs(instart,index-1,prestart+1,prestart+l);
+          }else
+              root.left=null;
+          if(r!=0)
+              root.right=dfs(index+1,inend,prestart+l+1,preend);
+          else
+              root.right=null;
+          
+          return root;
+        
+    }
 }
