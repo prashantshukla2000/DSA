ed14528de9c6bf921a8aa7cb6eccf5a4baca19fb
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-09-30 23:37:39 +0530

    Time: 110 ms (9.42%), Space: 72.1 MB (13.77%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.java
+++ 621-task-scheduler/621-task-scheduler.java
@@ -1,32 +1,33 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        HashMap<Character,Integer> map=new HashMap<>();
+     HashMap<Character,Integer> map=new HashMap();
         for(char c:tasks){
-           map.put(c,map.getOrDefault(c,0)+1);
+            map.put(c,map.getOrDefault(c,0)+1);
         }
-         PriorityQueue<Map.Entry<Character, Integer>> q = new PriorityQueue<>( //frequency sort
-            (a,b) -> a.getValue() != b.getValue() ? b.getValue() - a.getValue() : a.getKey() - b.getKey()); 
-    q.addAll(map.entrySet());
-    
-int count=0;
-    while(!q.isEmpty()){
-        int k = n + 1;
-        List<Map.Entry> tempList = new ArrayList<>();
-        while(k>0 &&!q.isEmpty()){
-          Map.Entry<Character, Integer> top = q.poll(); 
-             top.setValue(top.getValue() - 1);
-            tempList.add(top); 
-            k--;
-            count++;
-        }
-        for(Map.Entry<Character, Integer> e : tempList)
+        PriorityQueue<Map.Entry<Character,Integer>> queue=new PriorityQueue<>((a,b)-> a.getValue()!=b.getValue()?b.getValue()-a.getValue():a.getKey()-b.getKey());
+        queue.addAll(map.entrySet());
+        
+        int count=0;
+        while(!queue.isEmpty()){
+        int k=n+1;
+       List<Map.Entry> tempList = new ArrayList<>();
+            while(k>0 && !queue.isEmpty() )
+            {
+                Map.Entry<Character,Integer> top=queue.poll();
+                top.setValue(top.getValue()-1);
+                tempList.add(top);
+                k--;
+               count++; 
+            }
+      for(Map.Entry<Character, Integer> e : tempList) // add valid tasks
         {
             if(e.getValue()>0)
-                q.add(e);
+                queue.add(e);
         }
-        if (q.isEmpty()) break;
-        count=count+k;
-    }
-    return count;
+           if(queue.isEmpty()) break;
+            
+            count=count+k;
+        }
+        return count;
     }
 }
