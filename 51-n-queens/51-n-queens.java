16c2c3bcaee85151f1c7f260c0e67389fe7be219
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-10-04 20:11:58 +0530

    Time: 4 ms (81.98%), Space: 45.3 MB (56.72%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,46 +1,51 @@
-public class Solution {
+class Solution {
     public List<List<String>> solveNQueens(int n) {
-        char[][] board = new char[n][n];
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < n; j++)
-                board[i][j] = '.';
-        List<List<String>> res = new ArrayList<List<String>>();
-        dfs(board, 0, res);
+        char[][]board=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+        List<List<String>> res=new ArrayList();
+        dfs(board,res,0);
         return res;
     }
-    
-    private void dfs(char[][] board, int colIndex, List<List<String>> res) {
-        if(colIndex == board.length) {
+    public void dfs(char[][]board, List<List<String>> res,int i){
+        if(i==board.length){
             res.add(construct(board));
-            return;
+        return;
         }
-        
-        for(int i = 0; i < board.length; i++) {
-            if(validate(board, i, colIndex)) {
-                board[i][colIndex] = 'Q';
-                dfs(board, colIndex + 1, res);
-                board[i][colIndex] = '.';
+        for(int j=0;j<board.length;j++){
+            if(validate (board,i,j)){
+                board[i][j]='Q';
+                dfs(board,res,i+1);
+                board[i][j]='.';
             }
         }
     }
-    
-    private boolean validate(char[][] board, int x, int y) {
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < y; j++) {
-                if(board[i][j] == 'Q' && (x + j == y + i || x + y == i + j || x == i))
-                    return false;
+    public boolean validate(char[][]board, int row, int col){
+      for(int i=0;i<row;i++){
+          if(board[i][col]=='Q'){
+              return false;}
+      }  
+          int maxLeft = Math.min(row, col);
+        for (int i = 1; i <= maxLeft; i++) {
+            if (board[row - i][col - i]=='Q') {
+                return false;
             }
         }
-        
+        int maxRight = Math.min(row, board.length - 1 - col);
+        for (int i = 1; i <= maxRight; i++) {
+            if (board[row - i][col + i]=='Q') return false;
+        }
         return true;
-    }
     
-    private List<String> construct(char[][] board) {
-        List<String> res = new LinkedList<String>();
+    }
+      public List<String> construct(char[][] board) {//to add the list to the full list as string
+    List<String> res = new LinkedList<String>();//build solution from temp chessboard
         for(int i = 0; i < board.length; i++) {
             String s = new String(board[i]);
-            res.add(s);
-        }
-        return res;
-    }
+            res.add(s);}
+        return res; }
+
 }
