e32aa33680c0b1baf8141a15d4b156f69b90aba0
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-10-06 23:53:34 +0530

    Time: 4 ms (47.99%), Space: 51.8 MB (41.45%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -1,43 +1,35 @@
 class Solution {
-    int row=0;
-    int col=0;
     public void solve(char[][] board) {
-        row=board.length;
-        col=board[0].length;
-        //converting the outer "0" to temporary ->"T"
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-              if(board[i][j]=='O' &&((i==0 ||i==row-1)||(j==0 ||j==col-1)))
-                board= capture(i,j,board);
+        for(int i=0;i<board.length;i++ ){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]=='O'&&(i==0||i==board.length-1||j==0||j==board[0].length-1))
+                 dfs(board,i,j);
             }
         }
-                 
-             //    converting the rest of inside "0" to "X"
-                  for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                 if(board[i][j]=='O' )
-                     board[i][j]='X';
+         for(int r=0;r<board.length;r++ ){
+            for(int c=0;c<board[0].length;c++){
+                
+                    if(board[r][c]=='O'){
+                        board[r][c]='X';
+                    }
+                }
             }
-                 }
-                 //converting all the "T" back into "0" 
-                      for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                 if(board[i][j]=='T' )
-                     board[i][j]='O';
-            }
-                  }
         
-    }
-                 //using dfs
-    public  char[][] capture(int r,int c, char[][]board){
-        if(!(r<0 ||c<0 ||r>row-1||c>col-1||board[r][c]!='O'))
-        {
-        board[r][c]='T';
-        capture(r+1,c,board);
-        capture(r-1,c,board);
-        capture(r,c+1,board);
-        capture(r,c-1,board);
+             for(int i=0;i<board.length;i++ ){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]=='T')
+                    board[i][j]='O';
+            }
         }
-        return board;
     }
+    public void dfs(char[][]board,int r,int c){
+        if(r<0||c<0||r==board.length||c==board[0].length||board[r][c]!='O')
+            return;
+        board[r][c]='T';
+        dfs(board,r+1,c);
+        dfs(board,r-1,c);
+        dfs(board,r,c+1);
+        dfs(board,r,c-1);
+    }
+        
 }
