9093de92681df044fe83264f3f32e217e2bab03a
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-10-06 23:33:08 +0530

    Time: 84 ms (10.83%), Space: 72.7 MB (7.63%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,38 +1,37 @@
 class Solution {
-    int row=0,col=0;
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        row=heights.length;
-        col=heights[0].length;
-        Set<List<Integer>> pac=new HashSet<List<Integer>>();
+      List<List<Integer>> res=new ArrayList<>();
         Set<List<Integer>> alt=new HashSet<List<Integer>>();
-        for(int c=0;c<col;c++){
-        dfs(heights,0,c,pac,heights[0][c]);
-          dfs(heights,row-1,c,alt,heights[row-1][c]);
+        Set<List<Integer>>pac= new HashSet<List<Integer>>();
+        for(int r=0;r<heights.length;r++){
+            dfs(pac,r,0,heights,heights[r][0]);
+            dfs(alt,r,heights[0].length-1,heights,heights[r][heights[0].length-1]);
         }
-        for(int r=0;r<row;r++){
-            dfs(heights,r,0,pac,heights[r][0]);
-            dfs(heights,r,col-1,alt,heights[r][col-1]);
+        for(int c=0;c<heights[0].length;c++){
+            dfs(pac,0,c,heights,heights[0][c]);
+            dfs(alt,heights.length-1,c,heights,heights[heights.length-1][c]);
         }
-        List<List<Integer>> result=new ArrayList<List<Integer>>();
-        for(int r=0;r<row;r++){
-            for(int c=0;c<col;c++){
-                if(pac.contains(Arrays.asList(r,c))&&alt.contains(Arrays.asList(r,c))){
-                    result.add(Arrays.asList(r,c));
+        for(int r=0;r<heights.length;r++){
+            for(int c=0;c<heights[0].length;c++)
+            {    if(pac.contains(Arrays.asList(r,c))&&alt.contains(Arrays.asList(r,c))){
+                    res.add(Arrays.asList(r,c));
                 }
-            }
-                   }
-        return result;
-        }
 
-    
-    public void dfs(int[][]heights,int r, int c, Set<List<Integer>> set,int prevheight){
-        if(r<0||c<0||r==row||c==col||set.contains(Arrays.asList(r,c))||prevheight>heights[r][c])
-            return ;
+            }
+        }
+        return res;
+        
+    }
+    public void dfs(Set<List<Integer>> set,int r,int c,int[][]heights,int prev)
+    {
+        if(r<0||c<0||r==heights.length||c==heights[0].length|| prev> heights[r][c] ||set.contains(Arrays.asList(r,c)))
+            return;
         set.add(Arrays.asList(r,c));
-        dfs(heights,r+1,c,set,heights[r][c]);
-        dfs(heights,r-1,c,set,heights[r][c]);
-        dfs(heights,r,c+1,set,heights[r][c]);
-        dfs(heights,r,c-1,set,heights[r][c]);
+        prev=heights[r][c];
+        dfs(set,r+1,c,heights,prev);
+        dfs(set,r-1,c,heights,prev);
+        dfs(set,r,c+1,heights,prev);
+        dfs(set,r,c-1,heights,prev);
         
     }
 }
