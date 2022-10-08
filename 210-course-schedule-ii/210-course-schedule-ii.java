228a3f006b500ec07ddec7467308901697c1891d
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-10-08 20:34:50 +0530

    Time: 11 ms (59.80%), Space: 49.9 MB (35.54%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,39 +1,51 @@
 class Solution {
+
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int [] order=new int[numCourses];
-  int[][]matrix=new int[numCourses][numCourses];
-        int in[]=new int[numCourses];
-        for(int i[]:prerequisites){
-            int a=i[0];
-            int b=i[1];
-            in[a]++;
-            matrix[b][a]=1;
+        Map<Integer, List<Integer>> adjList = new HashMap<Integer, List<Integer>>();
+        int[] indegree = new int[numCourses];
+        int[] topologicalOrder = new int[numCourses];
+
+        //creating the adjlist
+        for (int i = 0; i < prerequisites.length; i++) {
+            int post = prerequisites[i][0];
+            int pre = prerequisites[i][1];
+            List<Integer> lst = adjList.getOrDefault(
+                pre,
+                new ArrayList<Integer>()
+            );
+            lst.add(post);
+            adjList.put(pre, lst);
+
+            indegree[post] += 1;
         }
-        Queue<Integer>q=new LinkedList<Integer>();
-        for(int i=0;i<numCourses;i++){
-            if(in[i]==0){
+
+        Queue<Integer> q = new LinkedList<Integer>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
                 q.add(i);
             }
         }
-        int count=0;
-        while(!q.isEmpty()){
-          int ele=q.poll();
-            order[count]=ele;
-            count++;
-            int size=q.size();
-            
-            for(int i=0;i<numCourses;i++){
-               if(matrix[ele][i]==1)
-                if(--in[i]==0)
-                {
-                    q.add(i);
+
+        int i = 0;
+        while (!q.isEmpty()) {
+            int node = q.remove();
+            topologicalOrder[i++] = node;
+
+            if (adjList.containsKey(node)) {
+                for (Integer neighbor : adjList.get(node)) {
+                    indegree[neighbor]--;
+
+                    if (indegree[neighbor] == 0) {
+                        q.add(neighbor);
+                    }
                 }
             }
         }
-        if(count==numCourses)
-        return order;
-        else
-            return new int[0];
-          
+
+        if (i == numCourses) {
+            return topologicalOrder;
+        }
+
+        return new int[0];
     }
 }
