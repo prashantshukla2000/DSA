b78598e08800ef204f7b344296b1a4ea68367258
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-10-09 00:11:51 +0530

    Time: 5 ms (28.38%), Space: 44.1 MB (41.89%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.java
+++ 684-redundant-connection/684-redundant-connection.java
@@ -1,27 +1,26 @@
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-        int parent[]=new int [1001];
-        for(int i=0;i<1001;i++){ // weset each parent for every element as node itself initially
-            parent[i]=i;
-        }
-        for (int edge[] :edges){
-            int node_a=edge[0];
-            int node_b=edge[1];
-            int par_a=node_a;
-            int par_b=node_b;
-    while(par_a!=parent[par_a]) { //up until we don't find the real parent for a node.we keep moving and checking with the parent array as for real parent, it itself will be its parent
-                par_a=parent[par_a];
+       int pr[]=new int[1001];
+       for(int i=0;i<1001;i++){
+           pr[i]=i;
+       } 
+        for(int edge[]:edges){
+            int a=edge[0];
+            int b=edge[1];
+            int par_a=a;
+            int par_b=b;
+            while(par_a!=pr[par_a]){
+                par_a=pr[par_a];
             }
-             while(par_b!=parent[par_b]){//simillar as the source node of the edge we do for the destination node
-                par_b=parent[par_b];
+             while(par_b!=pr[par_b]){
+                par_b=pr[par_b];
             }
-            if(par_a!=par_b)//means we have two disconnected forests
-            {
-                parent[par_a]=par_b; //we join them by setting the parent of frst node to that of second
-            }else{ //it means it is redundat that it has already occured and will form a loop
+            if(par_a==par_b)
                 return edge;
-            }
+            else
+                pr[par_a]=par_b;
         }
         return new int[2];
+        
     }
 }
