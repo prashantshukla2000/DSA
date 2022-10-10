1aaf8957acd610751e15cae808580b0accf2fe37
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-10-10 23:19:52 +0530

    Time: 218 ms (69.56%), Space: 114.8 MB (49.34%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,37 +1,34 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int n=points.length,ans=0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-  pq.add(new int[]{ Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]), i, j }); //finding the distance b/w two points
+        int n=points.length;int ans=0;
+      PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]); 
+        for(int i=0;i<n;i++)//run for all nodes node 0,node 1..node n
+        {
+            for(int j=i+1;j<n;j++){//calculating the mmanhatten dist for nodes in point
+                 pq.add(new int[]{ Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]), i, j });  
+                
             }
         }
-        
-        int parent[]=new int[n];//union find
-         for(int i = 0; i < n; i++){
-             parent[i] = i;
-    }
+        int parent[]=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
         while(n>1){
-             int[] edge = pq.poll();
-            int dis=edge[0];
-            int i=edge[1];
-            int j=edge[2];
-           int pari=parent[i];
-             int   parj=parent[j];
-            while(parent[pari]!=pari)
-                pari=parent[pari];
-          while(parent[parj]!=parj)
-                parj=parent[parj];
-            if(pari!=parj)
-            {
-                parent[pari]=parj;
-                   ans+=dis;
-                n--;
+          int a[]=  pq.poll();
+            int dist=a[0];
+            int i=a[1];
+            int j=a[2];
+            int par_i=parent[i];
+            int par_j=parent[j];
+            while(parent[par_i]!=par_i)par_i=parent[par_i];
+            while(parent[par_j]!=par_j)par_j=parent[par_j];
+            if(par_i!=par_j){
+                parent[par_i]=par_j;
+                ans=ans+dist;
+                 n--;
             }
-         
         }
         return ans;
-        
+
     }
 }
