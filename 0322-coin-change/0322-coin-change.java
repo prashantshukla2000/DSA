b6b969067ad57ee1977e4bf91e4cbda518d86399
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-10-14 01:24:11 +0530

    Time: 466 ms (5.03%), Space: 145.1 MB (5.20%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int coinChange(int[] arr, int x) {
+        int n=arr.length;
+       Queue<Integer> q = new LinkedList<>();
+    // Base value in queue
+    q.add(x); 
+    // Boolean array to check if  a number has been visited before
+    HashSet<Integer> v = new HashSet<Integer>();
+    // Variable to store depth of BFS
+    int d = 0;
+    while (q.size() > 0)
+    {
+        int s = q.size();
+        while (s-- > 0)
+        {
+            int c = q.peek();             // Front most element of the queue
+            // Base case
+            if (c == 0)
+                return d;
+            q.remove();
+            if (v.contains(c) || c < 0)
+                continue;
+            v.add(c);            // Setting current state as visited
+            for (int i = 0; i < n; i++)            // Pushing the required states in queue
+              q.add(c - arr[i]);
+        }
+        d++;
+    }
+    return -1; 
+
+    }
+}
