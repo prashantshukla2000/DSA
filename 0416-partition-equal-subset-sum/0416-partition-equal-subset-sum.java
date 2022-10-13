82027341bed2c10c49b74ad5283afea55c1f3b95
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-10-14 04:10:19 +0530

    Time: 236 ms (15.05%), Space: 80.6 MB (15.34%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,26 +1,21 @@
 class Solution {
-
     public boolean canPartition(int[] nums) {
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
-        }
-        if (sum % 2 != 0) return false;
-        int target = sum / 2;
-        boolean[][] dp = new boolean[nums.length + 1][target + 1];
-        int n = nums.length;
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= target; j++) {
-                if (i == 0 || j == 0) {
-                    if (i == 0) dp[i][j] = false; else if (j == 0) dp[i][j] =
-                        true;
-                } else if (j >= nums[i - 1]) {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
+ Set<Integer> dp=new HashSet<Integer>();
+        int sum=0;
+        for(int a:nums)
+            sum=sum+a;
+        if(sum%2==1)return false;
+        int target=sum/2;
+        dp.add(0);
+        for(int i=nums.length-1;i>=0;i--){
+        List<Integer> list = new ArrayList<>(dp); 
+            for(int t:list){
+                if(t+nums[i]==target)
+                    return true;
+                dp.add(t+nums[i]);
             }
         }
-        return dp[n][target];
+        if(dp.contains(target)) return true;
+      return false;
     }
 }
