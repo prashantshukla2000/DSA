8213039d4ce4ae409555ed13f0e6c367408ff445
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-10-17 02:37:33 +0530

    Time: 14 ms (44.47%), Space: 41.5 MB (79.34%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, 0);
+
+        // if amount is 0, there is only 1 way of making change (no money)
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int i = 1; i <= amount; i++) {
+                if (coin <= i) {
+                    dp[i] = dp[i] + dp[i - coin];
+                }
+            }
+        }
+
+        return dp[amount];
+    }
+}
