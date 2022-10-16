10ac881ac2411ca030bee594a46ed3da8c73dcaa
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-10-17 04:52:54 +0530

    Time: 13 ms (29.23%), Space: 42.5 MB (45.72%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,24 +1,25 @@
 class Solution {
-             int m ,n , t;
- public boolean isInterleave(String s1, String s2, String s3) {
-             Map<String,Boolean> cache = new HashMap<>();
-                     m = s1.length(); n = s2.length(); t = s3.length();
-             if(m+n != t) return false;
-        return helper(s1, s2, s3, 0, 0,cache);
-    }
-    
-public boolean helper(String s1, String s2, String s3, int i, int j,Map<String,Boolean > cache){
-   if(i==m&&j==n)
-       return true;
-     String key=i +"-"+j;
-        if(cache.containsKey(key))
-            return cache.get(key);
-    if(i<m&&s1.charAt(i)==s3.charAt(i+j)&&helper(s1,s2,s3,i+1,j,cache))
-        return true;
-       if(j<n&&s2.charAt(j)==s3.charAt(i+j)&&helper(s1,s2,s3,i,j+1,cache))
-        return true;
-    cache.put(key,false);
-    return false;
-    
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        dp[s1.length()][s2.length()] = true;
+
+        for (int i = dp.length - 1; i >= 0; i--) 
+            for (int j = dp[0].length - 1;j >= 0;j--) {
+            if (i < s1.length() &&s1.charAt(i) == s3.charAt(i + j) &&dp[i + 1][j]
+            ) {
+                dp[i][j] = true;
+            }
+            if (j < s2.length() && s2.charAt(j) == s3.charAt(i + j) && dp[i][j + 1]
+            ) {
+                dp[i][j] = true;
+            }
+        }
+
+        return dp[0][0];
     }
 }
