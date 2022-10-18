0ee72a9162d930eb7ff8305baed2db0998a64008
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-10-18 11:28:00 +0530

    Time: 62 ms (8.98%), Space: 51.9 MB (9.54%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,21 +1,15 @@
 class Solution {
-
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, 0);
-
-        // if amount is 0, there is only 1 way of making change (no money)
-        dp[0] = 1;
-
-        for (int coin : coins) {
-            for (int i = 1; i <= amount; i++) {
-                if (coin <= i) {
-                    dp[i] = dp[i] + dp[i - coin];
-                }
-            }
-        }
-
-        return dp[amount];
+        Integer[][] dp = new Integer[coins.length][amount + 1];
+        return dfs(coins, 0, amount, dp);
+    }
+    int dfs(int[] coins, int i, int amount, Integer[][] dp) {
+        if (amount == 0) return 1;
+        if (i == coins.length) return 0;
+        if (dp[i][amount] != null) return dp[i][amount];
+        int ans = dfs(coins, i + 1, amount, dp); // skip ith coin
+        if (amount >= coins[i])
+            ans += dfs(coins, i, amount - coins[i], dp); // use ith coin
+        return dp[i][amount] = ans;
     }
 }
