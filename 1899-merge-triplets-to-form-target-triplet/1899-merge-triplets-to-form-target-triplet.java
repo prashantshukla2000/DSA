5afcb128c6ab475c4f1ac346f89aea82d34757a7
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-10-20 02:25:45 +0530

    Time: 16 ms (25.24%), Space: 166.4 MB (23.07%) - LeetHub

---
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        int a=target[0];int b=target[1];int c=target[2];
+        boolean flag1=false,flag2=false,flag3=false;
+        for(int val[]:triplets){
+            if(val[0]>a||val[1]>b||val[2]>c)
+                continue;
+            if(val[0]==a)flag1=true;
+            if(val[1]==b)flag2=true;
+            if(val[2]==c)flag3=true;
+             
+        } if(flag1==true&&flag2==true&&flag3==true)
+            return true;
+      
+        return false;
+    }
+}
