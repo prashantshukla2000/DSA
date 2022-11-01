91b99a7a1d611069b4a1136e764f214b09590a08
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-11-01 16:16:48 +0530

    Added solution - LeetHub

---
+++ Pattern 1 - GFG/pattern-1.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printSquare(n);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printSquare(int n) {
+        // code here
+    for(int i=n-1;i>=0;i--){
+        for(int j=n-1;j>=0;j--)
+    {
+        System.out.print("* ");
+        }
+        System.out.println();
+        
+    }
+        
+    }
+}
