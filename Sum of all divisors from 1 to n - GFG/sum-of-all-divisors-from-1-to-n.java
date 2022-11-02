bb0be40ffe79076b7b17c81c97df5fbbb1e35455
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-11-02 19:23:53 +0530

    Added solution - LeetHub

--- Sum of all divisors from 1 to n - GFG/sum-of-all-divisors-from-1-to-n.java
+++ Sum of all divisors from 1 to n - GFG/sum-of-all-divisors-from-1-to-n.java
@@ -26,14 +26,12 @@ class GFG
 
 //User function Template for Java
 class Solution{
-    static long sumOfDivisors(int num){
+    static long sumOfDivisors(int n){
         // code here
-    long sum = 0;
-    for (long i = 1; i <= Math.sqrt(num); i++) {
-        long t1 = i * (num / i - i + 1); // adding i every time it appears with numbers greater than or equal to itself
-        long t2 = (((num / i) * (num / i + 1)) / 2) - ((i * (i + 1)) / 2); // adding numbers that appear with i and are greater than i
-        sum += t1 + t2;
-    }
+ 
+   long sum = 0;
+    for (long i = 1; i <= n; ++i)
+        sum += (n / i) * i;
     return sum;
     }
 }
