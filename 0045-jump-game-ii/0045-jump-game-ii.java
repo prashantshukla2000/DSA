836b312fb13a48946b15993e64d430915d2daae4
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-11-02 19:35:31 +0530

    Time: 6 ms (60.50%), Space: 49.5 MB (44.60%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,11 +1,10 @@
 class Solution {
     public int jump(int[] nums) {
-        int res=0;
-        int l=0,r=0;//intially we have only the frst elemeent in our indow
+       int r=0,l=0,res=0;
         while(r<nums.length-1){
-            int farthest=0;//finding what is the max distance we can jump
+            int farthest=0;
             for(int i=l;i<=r;i++){
-               farthest= Math.max(farthest,i+nums[i]);
+                farthest=Math.max(farthest,i+nums[i]);
             }
             l=r+1;
             r=farthest;
