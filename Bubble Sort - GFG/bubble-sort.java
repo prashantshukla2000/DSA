1f3faa85483441b13c0eb6daf4cd719693e88470
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-11-03 18:48:39 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.java
+++ Bubble Sort - GFG/bubble-sort.java
@@ -13,18 +13,42 @@ class Solution
     //Function to sort the array using bubble sort algorithm.
 	public static void bubbleSort(int arr[], int n)
     {
-        //code here
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-1;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                }
-            }
-        }
+        //base condition
+   if(n==0||n==1)
+   return;
+   //since we are soting from back by placing the larget ele first we can 
+   //check till n-1 only as we know after that are sorted
+   for(int i=0;i<n-1;i++){ 
+       if(arr[i]>arr[i+1]){
+           swap(arr,i,i+1);
+       }
+   }
+   bubbleSort(arr,n-1);
+   
+    }
+    public static void swap(int a[], int i, int j){
+        int tem=a[i];
+        a[i]=a[j];
+        a[j]=tem;
     }
 }
+// class Solution
+// {
+//     //Function to sort the array using bubble sort algorithm.
+// 	public static void bubbleSort(int arr[], int n)
+//     {
+//         //code here
+//         for(int i=0;i<n-1;i++){
+//             for(int j=0;j<n-i-1;j++){   //also works for n-1
+//                 if(arr[j]>arr[j+1]){
+//                     int temp=arr[j];
+//                     arr[j]=arr[j+1];
+//                     arr[j+1]=temp;
+//                 }
+//             }
+//         }
+//     }
+// }
 
 //{ Driver Code Starts.
 
