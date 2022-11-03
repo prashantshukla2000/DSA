fe4181bd3776b2411dfaadd7c31718fda275eda6
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-11-03 22:07:23 +0530

    Time: 2 ms (71.61%), Space: 47.9 MB (45.56%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,18 +1,32 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int count=1,j=1;
-        for(int i=0;i<nums.length-1;i++){
-        while(j<nums.length && nums[j]==nums[i]){
-            j++;
+        //Two pointers soltion..
+    int i=0;
+        for(int j=1;j<nums.length;j++){
+        if(nums[i]!=nums[j]){
+            i++;
+            nums[i]=nums[j];
         }
-        while(j<nums.length && nums[j]!=nums[i]){
-           nums[count]=nums[j];
-            count++;
-            i=j;
-            j++;
-        }
-        }
-        return count;
-        
+    }
+        return i+1;
     }
 }
+//class Solution {
+//     public int removeDuplicates(int[] nums) {
+//Sliding Window solution...
+//         int count=1,j=1;
+//         for(int i=0;i<nums.length-1;i++){
+//         while(j<nums.length && nums[j]==nums[i]){
+//             j++;
+//         }
+//         while(j<nums.length && nums[j]!=nums[i]){
+//            nums[count]=nums[j];
+//             count++;
+//             i=j;
+//             j++;
+//         }
+//         }
+//         return count;
+        
+//     }
+// }
