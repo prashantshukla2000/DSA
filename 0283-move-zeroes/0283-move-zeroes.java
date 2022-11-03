cb3fa6de05ac3b12b6771712bd4508181ab43f42
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-11-03 23:23:21 +0530

    Time: 8 ms (37.55%), Space: 54.7 MB (58.74%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,34 @@
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int c=0,j=0;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(nums[i]!=0){
+//                 nums[j]=nums[i];
+//                 j++;
+//             }else{
+//                 c++;
+//             }
+//         }
+//         for(int i=nums.length-c;i<nums.length;i++){
+//             nums[i]=0;
+//         }
+//     }
+// }
+
+//Snowball solution
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int c=0,j=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]!=0){
-                nums[j]=nums[i];
-                j++;
-            }else{
-                c++;
+     public void moveZeroes(int[] nums) {
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
             }
-        }
-        for(int i=nums.length-c;i<nums.length;i++){
-            nums[i]=0;
+          else if (snowBallSize > 0){//we swap the zero with the elements as we encounter them
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
+            }
         }
     }
 }
