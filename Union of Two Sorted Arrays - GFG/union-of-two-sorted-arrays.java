d50616d25636b70c7e5ad74c14522e8ac5012986
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-11-05 02:43:46 +0530

    Added solution - LeetHub

--- Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.java
+++ Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.java
@@ -54,21 +54,34 @@ class Solution
     //Function to return a list containing the union of the two arrays.
     public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
     {
-        TreeSet<Integer> set=new TreeSet<>();
-        ArrayList<Integer> ans=new ArrayList<>();
-        for(int i=0;i<arr1.length;i++){
-            set.add(arr1[i]);
-        }
-        
-        for(int i=0;i<arr2.length;i++){
-            set.add(arr2[i]);
-        }
-        Iterator<Integer> i = set.iterator();
- 
-
-        while (i.hasNext())
-            ans.add(i.next());
-        return ans;
+        int i = 0, j = 0; // pointers
+  ArrayList<Integer > Union=new ArrayList<>(); // Uninon vector
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.get(Union.size()-1) != arr1[i])
+        Union.add(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.get(Union.size()-1) != arr2[j])
+        Union.add(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.get(Union.size()-1) != arr1[i])
+      Union.add(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.get(Union.size()-1) != arr2[j])
+      Union.add(arr2[j]);
+    j++;
+  }
+  return Union;
     }
 }
 
