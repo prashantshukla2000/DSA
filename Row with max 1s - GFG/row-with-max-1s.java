fcd0a4b2301180608b2de368357bca927146fc71
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-11-05 23:11:03 +0530

    Added solution - LeetHub

--- Row with max 1s - GFG/row-with-max-1s.java
+++ Row with max 1s - GFG/row-with-max-1s.java
@@ -32,33 +32,52 @@ public class Main {
 
 
 //User function Template for Java
-
+//Using the way of serching...where we search each row from last ..f 1 found go more back in the same....else 
+//we go to down if any of the down rows have 1 in it...ie,they have more ones so we break out of that row to check down
 class Solution {
     int rowWithMax1s(int arr[][], int n, int m) {
         // code here
-        int max=Integer.MAX_VALUE,ans=-1;
+        int col=m-1,row=-1;
         for(int i=0;i<n;i++){
-            int index=bs(arr[i],0,m-1);
-           // System.out.println(index);
-            if(index != -1 && index<max ){
-                max=index;
-                ans=i;
-            }
+           for(int j=col;j>=0;j--){
+               if(arr[i][j]==1){
+                   col--;
+                   row=i;
+               }else{
+                   break;
+               }
+           }
         }
-        return ans;
-    }
-    int bs(int arr[],int low,int high){
-      if (high>=low){
-            int mid=(low+high)/2;
-            if((mid==0||arr[mid-1]==0)&&(arr[mid]==1)){
-            return mid;
-            }
-            else if(arr[mid]==0){
-             return   bs(arr,(mid+1),high);
-            }else{
-                return bs(arr,low,(mid-1));
-            }
-        }
-        return -1;
+        return row;
     }
 }
+    //OR Using Binary Serach
+
+// class Solution {
+//     int rowWithMax1s(int arr[][], int n, int m) {
+//         // code here
+//         int max=Integer.MAX_VALUE,ans=-1;
+//         for(int i=0;i<n;i++){
+//             int index=bs(arr[i],0,m-1);
+//             if(index != -1 && index<max ){
+//                 max=index;
+//                 ans=i;
+//             }
+//         }
+//         return ans;
+//     }
+//     int bs(int arr[],int low,int high){
+//       if (high>=low){
+//             int mid=(low+high)/2;
+//             if((mid==0||arr[mid-1]==0)&&(arr[mid]==1)){
+//             return mid;
+//             }
+//             else if(arr[mid]==0){
+//              return   bs(arr,(mid+1),high);
+//             }else{
+//                 return bs(arr,low,(mid-1));
+//             }
+//         }
+//         return -1;
+//     }
+// }
