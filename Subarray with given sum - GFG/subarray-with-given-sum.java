3da81d56301b003b442f248d921b0d8424018e95
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-11-14 03:44:39 +0530

    Added solution - LeetHub

---
+++ Subarray with given sum - GFG/subarray-with-given-sum.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(a, n, k);
+            for(int ii = 0;ii<res.size();ii++)
+                ot.print(res.get(ii) + " ");
+            ot.println();
+        }
+        ot.close();
+    }
+
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int sum) 
+    {
+        // Your code here
+      ArrayList<Integer> al=new ArrayList<>();
+     int currentSum = arr[0], start = 0, i;
+ 
+        // Pick a starting point
+        for (i = 1; i <= n; i++) {
+            // If currentSum exceeds the sum,
+            // then remove the starting elements
+            while (currentSum > sum && start < i - 1) {
+                currentSum = currentSum - arr[start];
+                start++;
+            }
+ 
+            // If currentSum becomes equal to sum,
+            // then return true
+            if (currentSum == sum) {
+                al.add(start+1);
+                al.add(i);
+                return al;
+            }
+ 
+            // Add this element to curr_sum
+            if (i < n)
+                currentSum = currentSum + arr[i];
+        }
+        al.add(-1);
+         return al;
+    }
+}
